96a8abbfd063a4830cfbcb4b85d6b8aa338a3361
jesszhang <jesscyz97@outlook.com>
2023-03-01 22:42:48 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,16 @@
+module "network" {
+  source  = "app.terraform.io/jess-freedom/network/google"
+  version = "3.4.0"
+  network_name = "jess-network"
+  project_id = "ws-b98f8ayYXe51343m"
+
+  # insert required variables here
+
 subnets = [
   {
-    subnet_name   = "gaurav-subnet"
+    subnet_name   = "jess-subnetwork"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+}
