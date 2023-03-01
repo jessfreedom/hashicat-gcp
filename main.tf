fbfbb50d31c8cd102d1ca21c1c0e8d953c312cdc
jesszhang <jesscyz97@outlook.com>
2023-03-01 22:27:08 +0000

    Added the first label

--- main.tf
+++ main.tf
@@ -49,7 +49,9 @@ resource "tls_private_key" "ssh-key" {
 resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
-  machine_type = var.machine_type
+  labels = {
+    department = "devops"
+  }  machine_type = var.machine_type
 
   boot_disk {
     initialize_params {
